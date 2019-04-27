3c847816d28e18c5c14b7d0c985b03c6b6286a5e
Your Name <you@example.com>
2019-04-27 01:31:26 +0100

    changes

--- build.gocd.groovy
+++ build.gocd.groovy
@@ -1,27 +1,47 @@
-import cd.go.contrib.plugins.configrepo.groovy.dsl.GoCD
+import cd.go.contrib.plugins.configrepo.groovy.dsl.*
 
 GoCD.script {
   pipelines {
-    pipeline('gocd-groovy-dsl-plugin') {
-      group = "tests"
+    pipeline('build') {
+      group = 'go-cd'
+      lockBehavior = 'lockOnFailure'
+      trackingTool {
+        link = 'https://github.com/ketan/gocd-groovy-dsl-config-plugin/issues/${ID}'
+        regex = ~/##(\d+)/
+      }
+
+      environmentVariables = [
+        'pipeline-var': 'pipeline-value'
+      ]
 
       materials {
-        git {
-          url = 'git@github.com:videocursoscloud/curso_gocd_2019.git'
+        git('my-repo') {
+          url = 'https://github.com/ketan/gocd-groovy-dsl-config-plugin'
+          branch = 'master'
+          blacklist = ['README.md', "examples/**/*"]
         }
       }
+
       stages {
-        stage('build') {
+        stage('test') {
+          environmentVariables = [
+            'stage-var': 'stage-value'
+          ]
           jobs {
             job('build') {
               tasks {
                 bash {
-                  commandString = 'echo hello world'
+                  commandString = "git clean -fdx"
+                }
+
+                bash {
+                  commandString = './gradlew clean assemble test'
                 }
               }
-           }
+            }
+          }
         }
-     }
+      }
+    }
   }
 }
-
